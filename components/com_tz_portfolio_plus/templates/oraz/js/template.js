1404ffe4ee033476c2e53292535f94d34ac58e92
Alex P <acryllect@gmail.com>
2018-12-29 18:09:12 +0100

    First Upload

---
+++ components/com_tz_portfolio_plus/templates/oraz/js/template.js
@@ -0,0 +1,12 @@
+(function($) {
+    "use strict";
+    $(document).ready(function() {
+        $('.orazLink').fancybox({
+            iframe : {
+                css : {
+                    width : '1200px'
+                }
+            }
+        });
+    });
+}(jQuery));
---
+++ templates/tz_oraz/js/page.js
@@ -0,0 +1,6 @@
+!function ($) {
+    "use strict";
+    $(window).load(function() {
+        $('body').find('.oraz-preloading').fadeOut(1000);
+    });
+}(jQuery);
